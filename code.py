e573341108ff4eec5c6e33b5facc96d08102dff5
Hosein Nekouei <nekouei.hosein@gmail.com>
2023-07-24 09:33:51 +0330

    First Commit

---
+++ code.py
@@ -0,0 +1,18 @@
+def square(x):
+    return x**2
+
+# call a function that call another function
+def my_map(func, input_list):
+    out_list = []
+
+    for item in input_list:
+        out_list.append(func(item))
+
+    return out_list
+
+
+
+input_list = [1, 2, 3, 4]
+
+result = my_map(square, input_list)
+print(result)
